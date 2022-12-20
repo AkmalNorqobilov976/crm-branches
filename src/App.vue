14302c11768f5cc229ed4bb31cc8998f02fe6a7c
AkmalNorqobilov976 <norqobilovakmal976@gmail.com>
2022-12-20 14:18:27 +0500

    first

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="border-green-400 border-3 text-red-300">
+    Good
+    <i class="ri-home-wifi-line"></i>
+    <div class="card">
+    <div class="flex flex-wrap md:justify-content-between justify-content-center card-container">
+        <div class="border-500 surface-overlay border-3 border-round font-bold m-2 flex align-items-center justify-content-center" style="min-width: 200px; min-height: 100px">border-500</div>
+        <div class="border-cyan-500 surface-overlay border-3 border-round font-bold m-2 flex align-items-center justify-content-center" style="min-width: 200px; min-height: 100px">border-cyan-500</div>
+        <div class="border-orange-500 surface-overlay border-3 border-round font-bold m-2 flex align-items-center justify-content-center" style="min-width: 200px; min-height: 100px">border-orange-500</div>
+    </div>
+</div>
+
+  </div>
 </template>
 
 <style lang="scss">
